1e8a9f7386e84b5401cabdcf9b8ef1959cb9e118
leeshubin <1364360027@qq.com>
2021-08-21 17:58:02 +0800

    idea 提交master change2

--- src/gittest.java
+++ src/gittest.java
@@ -2,5 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("hotfix commit");
+        System.out.println("master commit2");
     }
 }
